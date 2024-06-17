9dc7f9be5627fdabc09d01c0df0f00923365497b
Michael Krajca <148968907+Rajce007cz@users.noreply.github.com>
2024-06-17 15:01:13 +0200

    Update na hosting

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/krajca/components/';
+    public string $baseURL = 'https://webkrajca.zdenekhrdina.cz/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
